b378fe98ad2089b5da0f2934af92321129064195
Timmy <Timothywesr94@gmail.com>
2026-05-06 14:09:43 -0400

    Main Menu

---
+++ FQ2/MainMenu.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+namespace FQ2
+{
+    internal class MainMenu
+    {
+
+        public static void Menu()
+        {
+            string titleFile = "Title.txt";
+            string[] title = File.ReadAllLines(titleFile);
+
+            
+            foreach (string line in title)
+            {
+                Utilities.PrintColored(line, ConsoleColor.DarkRed);
+            }
+
+            Boolean KeepRunning = true;
+            while (KeepRunning) {
+                string playerInput = Console.ReadLine() ?? ("".ToLower());
+                switch (playerInput)
+                {
+                    case "e":
+                        Console.WriteLine("Work In Progress");
+                        break;
+                    case "q":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.Clear();
+                        Menu();
+                        Console.WriteLine("Invalid Input: Please type E or Q");
+                        
+
+                        break;
+                }
+
+            }
+
+        }
+
+
+
+    }
+}
---
+++ FQ2/Utilities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FQ2
+{
+    internal class Utilities
+    {
+        /// <summary>
+        /// Prints text in a specified color and resets to default color when finished.
+        /// </summary>
+        /// <param name="text"> The test to print</param>
+        /// <param name="color"> The color to print the text in</param>
+        public static void PrintColored(string text, ConsoleColor color)
+        {
+            Console.ForegroundColor =  color;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+
+    }
+}
